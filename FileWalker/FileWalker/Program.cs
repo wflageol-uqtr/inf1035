5921f88cb79b5d027fb4d63e6167b51ff2e62349
William Flageol <wflageol@gmail.com>
2021-11-18 17:06:45 -0500

    Ajout exemples semaine 11.

---
+++ FileWalker/FileWalker/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FileWalker
+{
+    interface IElement
+    {
+        string Name { get; }
+        void Accept(IVisitor visitor);
+    }
+
+    class Folder : IElement
+    {
+        private List<IElement> children = new List<IElement>();
+
+        public string Name { get; }
+        public Folder(string name)
+        {
+            Name = name;
+        }
+
+        public void Add(IElement childElement)
+        {
+            children.Add(childElement);
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            foreach(var child in children)
+            {
+                child.Accept(visitor);
+            }
+            visitor.Visit(this);
+        }
+    }
+
+    class File : IElement
+    {
+        public string Name { get; }
+        public File(string name)
+        {
+            Name = name;
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    interface IVisitor
+    {
+        void Visit(File file);
+        void Visit(Folder folder);
+    }
+
+    class PrintVisitor : IVisitor
+    {
+        public void Visit(File file)
+        {
+            Console.WriteLine(file.Name);
+        }
+
+        public void Visit(Folder folder)
+        {
+            Console.WriteLine("Folder: {0}", folder.Name);
+        }
+    }
+
+    class CountVisitor : IVisitor
+    {
+        public int Count { get; private set; }
+
+        public void Visit(File file)
+        {
+            Count++;
+        }
+
+        public void Visit(Folder folder)
+        {
+            
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var folder = new Folder("Folder 1");
+            var file = new File("readme.txt");
+            folder.Add(file);
+            var subfolder = new Folder("Folder 2");
+            folder.Add(subfolder);
+
+            var folder2 = new Folder("Folder 2");
+            var file2 = new File("context.txt");
+            folder2.Add(file2);
+
+            subfolder.Add(file2);
+
+            var visitor = new CountVisitor();
+            folder.Accept(visitor);
+            Console.WriteLine(visitor.Count);
+        }
+    }
+}
---
+++ MathParse/MathParse/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MathParse
+{
+    class Program
+    {
+        static IEnumerable<string> GeneratePostfix(string input)
+        {
+            var postfixStack = new Stack<string>();
+            var operatorStack = new Stack<string>();
+
+            var tokens = input.Split(' ');
+            postfixStack.Push(tokens[0]);
+
+            foreach(var token in tokens.Skip(1))
+            {
+                if (int.TryParse(token, out var _))
+                {
+                    postfixStack.Push(token);
+                    if (operatorStack.Any())
+                        postfixStack.Push(operatorStack.Pop());
+                } else
+                {
+                    operatorStack.Push(token);
+                }
+            }
+
+            return postfixStack.Reverse();
+        }
+
+        static int Calculate(IEnumerable<string> postfix)
+        {
+            var stack = new Stack<int>();
+
+            foreach(var token in postfix)
+            {
+                if(int.TryParse(token, out var i)) 
+                    stack.Push(i);
+                else
+                {
+                    var n1 = stack.Pop();
+                    var n2 = stack.Pop();
+
+                    int result;
+                    switch(token)
+                    {
+                        case "+":
+                            result = n2 + n1;
+                            break;
+                        case "-":
+                            result = n2 - n1;
+                            break;
+                        case "/":
+                            result = n2 / n1;
+                            break;
+                        case "*":
+                            result = n2 * n1;
+                            break;
+                        default:
+                            throw new InvalidOperationException();
+                    }
+
+                    stack.Push(result);
+                }
+            }
+
+            return stack.Pop();
+        }
+
+        static void PrintStack(IEnumerable<string> stack)
+        {
+            foreach (var element in stack)
+                Console.Write("{0} ", element);
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            var postfix = GeneratePostfix("5 + 8 * 2");
+            Console.WriteLine(Calculate(postfix));
+        }
+    }
+}
